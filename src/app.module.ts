3a8f9f3205134f21d1283b3bc531273a589b9268
Eduardo <corl.facturacion@gmail.com>
2024-07-24 11:22:32 -0600

    feat- add items- envConfig, joi validations & custom logguer

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,29 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { join } from 'path';
-import { ItemsModule } from './items/items.module';
+import { ItemsModule } from './core/items/items.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        database: configService.get<string>('DB_NAME'),
+        username: configService.get<string>('DB_USER_NAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       // playground: false,
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { CustomLoggerService } from 'src/shared/config/log';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new CustomLoggerService(),
+  });
   const port = 4000;
   app.enableCors();
   Logger.debug('Starting the application...');
   Logger.debug(`Starting in port ${port}`);
   await app.listen(4000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 }
 bootstrap();
