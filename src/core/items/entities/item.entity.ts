80cacd177e9b39c2d9c3cf7a2cf54435f221e25b
Eduardo <corl.facturacion@gmail.com>
2024-07-24 14:11:37 -0600

    FEAT- add items CRUD

--- src/core/items/entities/item.entity.ts
+++ src/core/items/entities/item.entity.ts
@@ -1,7 +1,27 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Field, Float, ID, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity({ name: 'items' })
 @ObjectType()
 export class Item {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  /**
+   * The unique identifier of the item.
+   */
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => ID)
+  id: string;
+
+  @Column()
+  @Field(() => String)
+  name: string;
+
+  @Column()
+  @Field(() => Float)
+  quantity: number;
+
+  @Column({ nullable: true })
+  @Field(() => String, {
+    nullable: true,
+  })
+  quantityUnits?: string; // e.g. 'kg', 'litre', 'each'
 }
